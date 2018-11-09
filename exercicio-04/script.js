caf85cd8b2d2f80ec41764fac13bc310ceb5908d
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-09 21:47:10 -0200

    Adicionar solução do quarto exercício

--- exercicio-04/script.js
+++ exercicio-04/script.js
@@ -2,7 +2,7 @@
 Crie um programa que identifique valores pares e ímpares. Esse programa deve:
 - Receber 10 valores numéricos e colocá-los em um vetor;
 - Iterar pelo vetor e definir se cada valor é par ou ímpar;
-- Mostrar ao usuário qual o valor sendo estudado e qual a definição dele;
+- Mostrar ao usuário qual o valor sendo estudado e qual a definição dele:
     Exemplo:
         - "1 é ímpar"
         - "38.219.830.922 é par"
@@ -10,3 +10,17 @@ Crie um programa que identifique valores pares e ímpares. Esse programa deve:
 - Bônus: Garantir que o usuário digite números nos inputs, repetindo a pergunta caso ele digite qualquer outra coisa que não números;
 */
 
+let valores = [];
+
+for (let i = 0; i < 10; i++) {
+  valores.push(Number(prompt('Digite um número:')));
+}
+
+for(const valor of valores) {
+    if (Math.floor(valor) % 2 == 0) {
+        console.log(`${valor} é par!`);
+    }
+    else {
+        console.log(`${valor} é ímpar!`);
+    }
+}

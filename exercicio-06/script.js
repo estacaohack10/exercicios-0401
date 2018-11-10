497039529d8ffb4ac54a86a5f0c4dbd666eac66a
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-09 22:07:13 -0200

    Adicionar solução do sexto exercício

--- exercicio-06/script.js
+++ exercicio-06/script.js
@@ -9,6 +9,17 @@ Crie um programa que escreva o seguinte output na tela:
 123****
 1234***
 12345**
+123456*
 1234567
 
 */
+
+let output = '*******';
+
+for (let i = 0; i <= 7; i++) {
+  if (i > 0) {
+    output = output.replace('*', i);
+  }
+
+  console.log(output);
+}
